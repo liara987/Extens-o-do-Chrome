80590364989ae83196ba56ed4a3e9d669d802ca2
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2025-08-20 21:25:14 -0300

    fix: add return to methods in getStrategy

--- fake-form-filler-br/content.js
+++ fake-form-filler-br/content.js
@@ -1,15 +1,12 @@
 const strategies = {
   sobrenome: (input, dados) => (input.value = dados.sobrenome || "sobrenome"),
-  primeiro_nome: (input, dados) =>
-    (input.value = dados.primeiroNome || "primeiroNome"),
-  nome_completo: (input, dados) =>
-    (input.value = dados.nomeCompleto || "nomeCompleto"),
+  primeiro_nome: (input, dados) => (input.value = dados.primeiroNome || "primeiroNome"),
+  nome_completo: (input, dados) => (input.value = dados.nomeCompleto || "nomeCompleto"),
   mae: (input, dados) => (input.value = dados.mae || "mae"),
   pai: (input, dados) => (input.value = dados.pai || "pai"),
   cpf: (input, dados) => (input.value = dados.cpf || "529.982.247-25"),
   rg: (input, dados) => (input.value = dados.rg || "18.237.365-4"),
-  nascimento: (input, dados) =>
-    (input.value = dados.nascimento || new Date.toISOString().slice(0, 10)),
+  nascimento: (input, dados) => (input.value = dados.nascimento || new Date.toISOString().slice(0, 10)),
   endereco: (input, dados) => (input.value = dados.endereco || "Rua Exemplo"),
   numero: (input, dados) => (input.value = dados.numero || "123"),
   bairro: (input, dados) => (input.value = dados.bairro || "Bairro Exemplo"),
@@ -32,15 +29,13 @@ const strategies = {
   radio: (input, dados) => {
     const sexo = (dados.sexo || "").toLowerCase();
 
-    document
-      .querySelectorAll(`input[type="radio"][name="${input.name}"]`)
-      .forEach((radio) => {
-        const val = radio.value.toLowerCase();
+    document.querySelectorAll(`input[type="radio"][name="${input.name}"]`).forEach((radio) => {
+      const val = radio.value.toLowerCase();
 
-        if (isGenderMatch(sexo, val)) {
-          radio.checked = true;
-        }
-      });
+      if (isGenderMatch(sexo, val)) {
+        radio.checked = true;
+      }
+    });
   },
   select: (input) => {
     const options = Array.from(input.options);
@@ -70,18 +65,12 @@ function isFullNameField(ref) {
 
 function isFirstNameField(ref) {
   return (
-    ref.includes("primeiro") ||
-    ((ref.includes("nome") || ref.includes("name")) &&
-      !ref.includes("sobrenome"))
+    ref.includes("primeiro") || ref.includes("first") || ((ref.includes("nome") || ref.includes("name")) && !ref.includes("sobrenome"))
   );
 }
 
 function isAddressField(ref) {
-  return (
-    ref.includes("endereco") ||
-    ref.includes("logradouro") ||
-    ref.includes("address")
-  );
+  return ref.includes("endereco") || ref.includes("logradouro") || ref.includes("address");
 }
 
 function isPasswordField(ref) {
@@ -95,68 +84,65 @@ function isStateField(ref) {
 function isGenderMatch(sexo, val) {
   const masc = ["m", "masc", "masculino"];
   const fem = ["f", "fem", "feminino"];
-  return (
-    (masc.includes(sexo) && masc.includes(val)) ||
-    (fem.includes(sexo) && fem.includes(val))
-  );
+  return (masc.includes(sexo) && masc.includes(val)) || (fem.includes(sexo) && fem.includes(val));
 }
 
 function isPhoneField(input) {
-  return (
-    input.type === "tel" || input.id.includes("tel") || input.id.includes("mob")
-  );
+  return input.type === "tel" || input.id.includes("tel") || input.id.includes("mob");
 }
 
 function fillByRef(input, ref) {
   if (strategies[input.type]) return strategies[input.type];
+
   if (isFullNameField(ref)) return strategies.nome_completo;
-
-  if (ref.includes("sobrenome") || ref.includes("lastname"))
-    return strategies.sobrenome;
-  if (isFirstNameField(ref)) return strategies.primeiro_nome;
-
+  else {
+    if (ref.includes("sobrenome") || ref.includes("lastname")) return strategies.sobrenome;
+    if (isFirstNameField(ref)) return strategies.primeiro_nome;
+  }
   if (ref.includes("mae") || ref.includes("mother")) return strategies.mae;
   if (ref.includes("pai") || ref.includes("father")) return strategies.pai;
   if (ref.includes("cpf")) return strategies.cpf;
   if (ref.includes("rg")) return strategies.rg;
-  if (ref.includes("nasc") || ref.includes("birth"))
-    return strategies.nascimento;
-  if (ref.includes("numero") || ref.includes("number"))
-    return strategies.numero;
-  if (ref.includes("bairro") || ref.includes("neighborhood"))
-    return strategies.bairro;
+  if (ref.includes("nasc") || ref.includes("birth")) return strategies.nascimento;
+  if (ref.includes("numero") || ref.includes("number")) return strategies.numero;
+  if (ref.includes("bairro") || ref.includes("neighborhood")) return strategies.bairro;
   if (ref.includes("cidade") || ref.includes("city")) return strategies.cidade;
   if (ref.includes("cep")) return strategies.cep;
   if (ref.includes("tel")) return strategies.telefone;
-  if (ref.includes("celular") || ref.includes("cell"))
-    return strategies.celular;
+  if (ref.includes("celular") || ref.includes("cell")) return strategies.celular;
   if (ref.includes("email")) return strategies.email;
   if (ref.includes("idade") || ref.includes("age")) return strategies.idade;
   if (ref.includes("signo") || ref.includes("zodiac")) return strategies.signo;
-  if (ref.includes("altura") || ref.includes("height"))
-    return strategies.altura;
+  if (ref.includes("altura") || ref.includes("height")) return strategies.altura;
   if (ref.includes("peso") || ref.includes("weight")) return strategies.peso;
-  if (ref.includes("tipo sanguineo") || ref.includes("blood type"))
-    return strategies.tipoSanguineo;
+  if (ref.includes("tipo sanguineo") || ref.includes("blood type")) return strategies.tipoSanguineo;
   if (ref.includes("cor") || ref.includes("color")) return strategies.cor;
+  if (ref.includes("senha") || ref.includes("password")) return strategies.senha;
   if (isAddressField(ref)) return strategies.endereco;
   if (isStateField(ref)) return strategies.estado;
-  if (isPasswordField(ref)) return strategies.senha;
 }
 
-function fallbackByType(input) {
+function filledByType(input) {
   if (isPhoneField(input)) return strategies.celular;
-  if (input.type === "text" || input.tagName.toLowerCase() === "textarea") {
+
+  if (input.type === "text") {
     return strategies.input;
   }
+
+  if (input.tagName.toLowerCase() === "textarea") {
+    return strategies.textarea;
+  }
+
   if (String(input.tagName).toLowerCase() === "select") {
     return strategies.select;
   }
 }
 
 function getStrategy(input, ref) {
-  fillByRef(input, ref);
-  fallbackByType(input);
+  const byRef = fillByRef(input, ref);
+  const byType = filledByType(input);
+  if (byRef) return byRef;
+  if (byType) return byType;
   return null;
 }
 
@@ -167,12 +153,7 @@ window.addEventListener("message", (event) => {
   const inputs = document.querySelectorAll("input, textarea, select");
 
   inputs.forEach((input) => {
-    const ref = (
-      input.name ||
-      input.id ||
-      input.placeholder ||
-      ""
-    ).toLowerCase();
+    const ref = (input.name || input.id || input.placeholder || "").toLowerCase();
     const strategy = getStrategy(input, ref);
     if (strategy) {
       strategy(input, dados);
